984fcb63d698cebf2e7bc3a35c7354906bdd5fee
Treasure Uzondu <uzondut@gmail.com>
2025-10-02 19:33:56 -0400

    added UUID to Account

--- src/main/java/org/virtuoso/escape/model/account/Account.java
+++ src/main/java/org/virtuoso/escape/model/account/Account.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.UUID;
 
 /**
  * @author Treasure
@@ -12,15 +13,18 @@ import java.security.SecureRandom;
 public class Account {
     private String hashedPassword;
     private String username;
+	private UUID id;
     private Score highScore;
 
     public Account(String username, String password) {
         this.username = username;
         this.hashedPassword = hashPassword(password);
-    }
+		this.id = UUID.randomUUID();
+	}
 
-    public Account(String username, String password, Score highScore) {
+    public Account(String username, String password, UUID id, Score highScore) {
         this(username, password);
+		this.id = id;
         this.highScore = highScore;
     }
 
@@ -32,22 +36,30 @@ public class Account {
         return this.username;
     }
 
+	public UUID getId(){
+		return this.id;
+	}
+
     public Score getHighScore() {
         return this.highScore;
     }
 
-    private String hashPassword(String password) {
-        try {
-            SecureRandom random = new SecureRandom();
-            byte[] salt = new byte[16];
-            random.nextBytes(salt);
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update(salt);
-            byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
-            return String.valueOf(hash);
-        } catch (NoSuchAlgorithmException e) {
-            // TODO(gabri) come up with a more elegant way to proceed.
-            throw new RuntimeException(e);
-        }
-    }
+	public static String hashPassword(String password) {
+		try {
+			SecureRandom random = new SecureRandom();
+			byte[] salt = new byte[16];
+			random.nextBytes(salt);
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			md.update(salt);
+			byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+			return String.valueOf(hash);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO(gabri) come up with a more elegant way to proceed.
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getHashedPassword(){
+		return this.hashedPassword;
+	}
 }
