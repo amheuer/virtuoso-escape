39cd2777bf2edb54ca41884da815842f96ecada9
Treasure Uzondu <uzondut@gmail.com>
2025-10-02 11:59:21 -0400

    Created Entity stub. ++ fixed duplicated access modifier in Chain. ++ extra files.

--- src/main/java/org/virtuoso/escape/model/Entity.java
+++ src/main/java/org/virtuoso/escape/model/Entity.java
@@ -1,1 +1,13 @@
 package org.virtuoso.escape.model;
+
+public class Entity{
+	private String id;
+
+	public Entity(String id){
+		this.id = id;
+	}// End of constructor
+
+	public String getId(){
+		return this.id;
+	}// End of getId
+}// End of Entity
--- src/main/java/org/virtuoso/escape/model/actions/Chain.java
+++ src/main/java/org/virtuoso/escape/model/actions/Chain.java
@@ -1,6 +1,5 @@
 package org.virtuoso.escape.model.actions;
 public class Chain implements Action{
-    public
     @Override
     public String execute() {
         return "";
