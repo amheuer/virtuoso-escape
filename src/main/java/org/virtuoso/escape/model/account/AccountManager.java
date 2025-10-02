56d2129803ce5d8406064590b5e6053d339df633
Treasure Uzondu <uzondut@gmail.com>
2025-10-02 19:57:22 -0400

    implemented AccountManager methods; closes #7. ++ added getHashedPassword() method and UUID in Account

--- src/main/java/org/virtuoso/escape/model/account/AccountManager.java
+++ src/main/java/org/virtuoso/escape/model/account/AccountManager.java
@@ -1,31 +1,49 @@
 package org.virtuoso.escape.model.account;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 /**
+ * @author Treasure
  * @author gabri
  */
 public class AccountManager {
- // TODO(gabri) add logic
     private static AccountManager accountManager;
     private HashMap<String, Account> accounts;
 
-    AccountManager() {
+    private AccountManager() {
+		this.accounts = new HashMap<>();
+	    HashMap<String, String> accts = DataLoader.loadAccounts();
 
+		for (Map.Entry<String, String> account : accts.entrySet()){
+			String username = account.getKey();
+			String password = account.getValue();
+			this.accounts.put(username, new Account(username, password));
+		}
     }
+
     public static AccountManager getInstance(){
-        return null;
+        if (accountManager == null){
+			accountManager = new AccountManager();
+        }
+		return accountManager;
     }
 
     public Optional<Account> login(String username, String password) {
-        return null;
-    }
-    public Optional<Account> newAccount(String username, String password){
-        return null;
-    }
-    public void logout() {
-        return;
+        Account account = this.accounts.get(username);
+		String hashedPassword = Account.hashPassword(password);
+		if (account != null && account.getHashedPassword().equals(hashedPassword)){
+			return Optional.of(account);
+		}
+		return Optional.empty();
     }
 
+    public Optional<Account> newAccount(String username, String password){
+        return Optional.of(new Account(username, password));
+    }
+
+    public void logout() {
+        DataWriter.saveAccounts();
+    }
 }
