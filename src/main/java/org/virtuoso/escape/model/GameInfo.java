c6f4059609ab4fd035aa31674f6e91c5bd116042
Andrew Heuer <amheuer4@gmail.com>
2025-10-03 12:19:43 -0400

    Added stubs for gameinfo and gamestate, finished gameInfo; closes #15; closes #14; closes #13

--- src/main/java/org/virtuoso/escape/model/GameInfo.java
+++ src/main/java/org/virtuoso/escape/model/GameInfo.java
@@ -1,4 +1,23 @@
 package org.virtuoso.escape.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GameInfo {
+    private static GameInfo instance;
+    private HashMap<String,HashMap<String,String>> language = new HashMap<String,HashMap<String,String>>();
+    public ArrayList<Floor> building = new ArrayList<Floor>();
+
+    public static GameInfo getInstance() {
+        if (instance == null) 
+            instance = new GameInfo();
+        return instance;
+    }
+
+    private GameInfo() {};
+
+    public String getString(String id, String stringId) {
+        return language.get(id).get(stringId);
+    }
 
 }
--- src/main/java/org/virtuoso/escape/model/GameState.java
+++ src/main/java/org/virtuoso/escape/model/GameState.java
@@ -1,4 +1,84 @@
 package org.virtuoso.escape.model;
-public class GameState {
 
+import java.time.Duration;
+import java.util.ArrayList;
+
+import org.virtuoso.escape.model.account.Account;
+
+public class GameState {
+    private static GameState instance;
+    private Floor currentFloor;
+    private Entity currentEntity;
+    private ArrayList<Item> currentItems;
+    private Duration time;
+    private Account account;
+
+    private Difficulty difficulty;
+    private String currentMessage;
+
+    public static GameState getInstance() {
+        if (instance == null) 
+            instance = new GameState();
+        return instance;
+    }
+
+    private GameState() {};
+
+    public void begin(Account account){
+        
+    }
+
+    public boolean hasItem(Item item){
+        return currentItems.contains(item);
+    }
+
+        public Floor getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(Floor currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public Entity getCurrentEntity() {
+        return currentEntity;
+    }
+
+    public void setCurrentEntity(Entity currentEntity) {
+        this.currentEntity = currentEntity;
+    }
+
+    public ArrayList<Item> getCurrentItems() {
+        return currentItems;
+    }
+
+    public Duration getTime() {
+        return time;
+    }
+
+    public void setTime(Duration time) {
+        this.time = time;
+    }
+
+    p
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public String getCurrentMessage() {
+        return currentMessage;
+    }
+
+    public void setCurrentMessage(String currentMessage) {
+        this.currentMessage = currentMessage;
+    }
+
+    public void write() {
+
+    }
 }
